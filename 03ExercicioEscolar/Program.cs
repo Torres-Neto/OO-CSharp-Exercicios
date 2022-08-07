8b99d9578908f55979ca49c4697f47b19f486eac
Torres-Neto <torresneto.tn@gmail.com>
2022-08-06 22:40:29 -0300

    exercício de fixação

---
+++ 03ExercicioEscolar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03ExercicioEscolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar objeto da classe Aluno
+            Aluno a = new Aluno();
+            a.nome = "Denis";
+            a.nota1 = 8;
+            a.nota2 = 10;
+            a.mensagem();
+        }
+    }
+}
